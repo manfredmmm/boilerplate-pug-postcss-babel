dde3898beef532d52423dfb7018782455e667564
manfred <manfredmiravitllasmas@gmail.com>
2017-02-28 11:29:34 +0100

    Webpack config has been updated

--- webpack.config.js
+++ webpack.config.js
@@ -6,16 +6,35 @@ module.exports = {
     path: `${__dirname}/dist`,
     filename: 'bundle.js'
   },
-  resolve: {
-    extensions: ['.js']
-  },
+  devtool: 'source-map',
   module: {
-    loaders: [
-      { test: /\.(woff|woff2)$/, loaders: ['url-loader?name=webfonts/[name].[ext]'] },
-      { test: /\.(png|jpg|jpeg|svg)$/, loaders: ['file-loader?name=images/[name].[ext]'] },
-      { test: /\.pug$/, loaders: ['pug-loader'] },
-      { test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader', 'eslint-loader'] },
-      { test: /\.css$/, loaders: ['style-loader', 'css-loader', 'postcss-loader'] }
+    rules: [
+      {
+        test: /\.(woff|woff2)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            name: 'webfonts/[name].[ext]'
+          }
+        }]
+      }, {
+        test: /\.(png|jpg|jpeg|svg)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: 'images/[name].[ext]'
+          }
+        }]
+      }, {
+        test: /\.pug$/,
+        use: ['pug-loader']
+      }, {
+        test: /\.js$/,
+        loader: ['babel-loader', 'eslint-loader']
+      }, {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader']
+      }
     ]
   },
   plugins: [
